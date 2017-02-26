bf33a7dfbe34340d35ef689141e25ce59b7622b8
Daniel Fernández Pérez <dan.fernandez.perez@gmail.com>
2017-02-26 18:18:50 +0000

    Added funtion to meassure time, error management
    Funtion added to meassure how long does it take to get the password.
    Inproved documentation and error management

--- pass_cracker.py
+++ pass_cracker.py
@@ -13,7 +13,7 @@
 ## getopt for parsing coomand line arguments
 ## win32.com for using windows applications, external library, requires instalation
 
-import sys, getopt, win32com.client
+import sys, getopt, time, win32com.client
 
 # displayHelp - Function to display usage help
 # input:
@@ -24,6 +24,51 @@ import sys, getopt, win32com.client
 def displayHelp():
 	print ('Usage: pass_cracker.py -w <wordlist> -f <file>')
 	print ('When specifying the file, enter full path.')
+
+# timeit - Function to calculate the time a given function takes
+# input
+## funtion to monitor
+# output
+## message on console with time function took to execute
+# Note: function adapted from 
+# http://codereview.stackexchange.com/questions/151928/random-password-cracker-using-brute-force
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print ('The password has been found in {:.5f} seconds.'.format(time.time() - start))
+        return result
+    return wrapper
+
+# getPass - Function get password from file with a given dictionary	
+# input:
+## word, object with MS Word functions
+## filename, file to open
+## passwords, all passwords from wordlist file
+## results,file to store password
+# output:
+## if password is found, it is stored in results file. Message on console.
+
+@timeit
+def getPass(word, filename, passwords, results):
+	found = False
+	
+	for password in passwords:
+		if (found == False):
+			try:
+				word.Documents.Open(filename, False, True, None, password)
+				if (word.Documents[0].Content):
+					found = True
+					print('Password found: ' + password)
+					print('Password has been stored in results.txt file.')
+					results.write(password)
+					results.close()
+			except:
+				#no output to be displayed
+				pass
+		else:
+			break
 	
 # main - Function to handle the main functionality of the program
 # input:
@@ -34,7 +79,6 @@ def displayHelp():
 def main(argv):
 	wordlist = ''
 	filename = ''
-	found = False
 	
 	try:
 		word = win32com.client.Dispatch("Word.Application")
@@ -61,14 +105,14 @@ def main(argv):
 				displayHelp()
 				sys.exit(1)
 			elif opt.lower() in ("-w","--wordlist"):
-				wordlist = arg.lower()
+				wordlist = arg
 			elif opt.lower() in ("-f","--file"):
-				filename = arg.lower()
+				filename = arg
 	
 	try:
 		open(filename, 'r')
 	except:
-		print('Cannot open file')
+		print('Cannot access file provided.')
 		sys.exit(2)
 		
 	try:
@@ -77,32 +121,22 @@ def main(argv):
 		password_file.close()
 		passwords = [item.rstrip('\n') for item in passwords]
 	except:
-		print('Cannot open wordlist')
+		print('Cannot open wordlist.')
 		sys.exit(2)
 		
 	try:
 		results = open('results.txt', 'w')
 	except:
-		print('Cannot open/create results doc')
+		print('Cannot create/modify results doc.')
 		sys.exit(2)
 		
-	for password in passwords:
-		if (found == False):
-			#print(password)
-			try:
-				wb = word.Documents.Open(filename, False, True, None, password)
-				if (word.Documents[0].Content):
-					found = True
-					print('Password found: ' + password)
-					print('Password has been stored in results.txt file.')
-					results.write(password)
-					results.close()
-			except:
-				#no output to be displayed
-				pass
-		else:
-			break
+	print('Trying to open file: ' + '"' + filename + '"')
+	print('With dictionary: ' +  '"' + wordlist + '"')
+
+	getPass(word, filename, passwords, results)
+	
 	sys.exit(1)
 	
 if __name__ == "__main__":
+	# First argument is the name of the program, so it can be ignored
 	main(sys.argv[1:])
